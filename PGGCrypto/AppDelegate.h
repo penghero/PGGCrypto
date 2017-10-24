04868a6dbcc64d42b01526573c567f79aa6cb35d
penghero <chen@chenpengs-MacBook-Air.local>
2017-10-24 08:07:57 +0800

    Initial Commit

---
+++ PGGCrypto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PGGCrypto
+//
+//  Created by 陈鹏 on 2017/10/24.
+//  Copyright © 2017年 penggege.CP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PGGCrypto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PGGCrypto
+//
+//  Created by 陈鹏 on 2017/10/24.
+//  Copyright © 2017年 penggege.CP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
